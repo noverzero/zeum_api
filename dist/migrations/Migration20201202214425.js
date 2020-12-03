a14044c05d08e04703186dfe7e8126d425f0e44a
dustinhuth <dustin@undefinedindustries.com>
2020-12-02 18:48:54 -0700

    creating CRUD routes for Posts table

--- dist/migrations/Migration20201202214425.js
+++ dist/migrations/Migration20201202214425.js
@@ -9,14 +9,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Migration20201202050047 = void 0;
+exports.Migration20201202214425 = void 0;
 const migrations_1 = require("@mikro-orm/migrations");
-class Migration20201202050047 extends migrations_1.Migration {
+class Migration20201202214425 extends migrations_1.Migration {
     up() {
         return __awaiter(this, void 0, void 0, function* () {
-            this.addSql('create table "post" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" text not null);');
         });
     }
 }
-exports.Migration20201202050047 = Migration20201202050047;
-//# sourceMappingURL=Migration20201202050047.js.map
+exports.Migration20201202214425 = Migration20201202214425;
+//# sourceMappingURL=Migration20201202214425.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import "reflect-metadata"
 import { MikroORM} from "@mikro-orm/core"
 import { __prod__ } from "./constants"
-import { Post } from "./entities/Post"
+//import { Post } from "./entities/Post"
 import microConfig from "./mikro-orm.config"
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello"
+import { PostResolver } from "./resolvers/post"
 
 const main = async () => {
 
@@ -12,6 +18,24 @@ const main = async () => {
     // await orm.em.persistAndFlush(post)
     // const posts = await orm.em.find(Post, {})
     // console.log(posts)
+    const app = express()
+    
+    // app.get('/', (_, res) => {
+    //     res.send("hello, world!")
+    // })
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app })
+    app.listen(2021, ()=>{
+        console.log('server started on localhost:2021')
+    })
 }
 
 main()
