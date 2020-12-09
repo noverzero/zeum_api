bbff5633e69180a82e00da46286f4ea8f22f3101
dustinhuth <dustin@undefinedindustries.com>
2020-12-09 01:03:35 -0700

    added error handlling for password and username basic.

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -70,13 +70,44 @@ UserResponse = __decorate([
 let UserResolver = class UserResolver {
     register(options, { em }) {
         return __awaiter(this, void 0, void 0, function* () {
+            if (options.username.length <= 2) {
+                return {
+                    errors: [{
+                            field: 'username',
+                            message: 'length must be greater than 2 characters',
+                        },]
+                };
+            }
+            if (options.password.length <= 5) {
+                return {
+                    errors: [{
+                            field: 'password',
+                            message: 'length must be greater than 5 characters',
+                        },]
+                };
+            }
             const hashedPassword = yield argon2_1.default.hash(options.password);
             const user = em.create(User_1.User, {
                 username: options.username,
                 password: hashedPassword,
             });
-            yield em.persistAndFlush(user);
-            return user;
+            try {
+                yield em.persistAndFlush(user);
+            }
+            catch (err) {
+                if (err.code == '23505') {
+                    return {
+                        errors: [
+                            {
+                                field: "username",
+                                message: "username already exists"
+                            }
+                        ]
+                    };
+                }
+                console.log("message: ", err);
+            }
+            return { user };
         });
     }
     login(options, { em }) {
@@ -110,7 +141,7 @@ let UserResolver = class UserResolver {
     }
 };
 __decorate([
-    type_graphql_1.Mutation(() => User_1.User),
+    type_graphql_1.Mutation(() => UserResponse),
     __param(0, type_graphql_1.Arg('options')),
     __param(1, type_graphql_1.Ctx()),
     __metadata("design:type", Function),
